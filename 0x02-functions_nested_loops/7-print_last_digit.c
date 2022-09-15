930349befbdc5b9ca13d71cc6b09159c9fba5f76
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-09-14 21:45:44 -0700

    Correct error overflow

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,21 @@
 
 int print_last_digit(int val)
 {
-	if (val < 0)
+	if (val < 0 && val > INT_MAX)
 	{
 		int chg = val * -1;
 		int nlast = chg % 10;
 
 		_putchar(nlast + '0');
 		return (nlast);
+	} else if (val == INT_MAX)
+	{
+		int ovlas = (val * - 1) - 1;
+		int nlast = (ovlas % 10) + 1;
+
+		_putchar(nlast + '0');
+		return (nlast);
+
 	} else if (val == 0)
 	{
 		_putchar('0');
