26c076a159c59b1c962c08b362ffce8dd7e80241
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-08 17:24:34 +0000

    Commit 0x0B directory with README and 0-create_array program

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_array - creates array of sixe int and initializes withh char
+ *
+ * @size: size of array passed to function, integer
+ * @c: initialization character
+ * Return: Pointer to array(success), NULL (fail)
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i = 0;
+
+	if (c == '\0' || size < 1)
+	{
+		return (NULL);
+	}
+
+	arr = (char *)malloc(size * sizeof(char));
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+#0x0B-malloc_free
+#
+#
+##Description
+#
+#
+##Resources
