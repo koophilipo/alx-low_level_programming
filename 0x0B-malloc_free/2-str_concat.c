4b8daa35b857ea56b5a853a3698952dcbc0e6e12
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-09 19:34:26 +0000

    Correct error in  2-str_concat program

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,67 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string array
+ *
+ * @val: string pointer
+ * Return: Returns length of array.
+ */
+
+int _strlen(char *val)
+{
+	int len = 0;
+
+	if (val != NULL)
+	{
+		while (val[len] != '\0')
+		{
+			len++;
+		}
+	}
+	return (len);
+}
+
+/**
+ * concat - appends string 2 to string 1
+ *
+ * @str1: string pointer
+ * @str2: string pointer
+ */
+
+void concat(char *str1, char *str2)
+{
+	int u = 0;
+	int o = _strlen(str1);
+
+	while (str2[u] != '\0')
+	{
+		str1[o] = str2[u];
+		o++;
+		u++;
+	}
+	str1[o] = '\0';
+}
+
+/**
+ * strcp - duplicates a string
+ *
+ * @val1: string pointer
+ * @val2: string pointer
+ */
+
+void strcp(char *val1, char *val2)
+{
+	int chk = 0;
+
+	while (val2[chk] != '\0')
+	{
+		val1[chk] = val2[chk];
+		chk++;
+	}
+}
+
+
+/**
  * str_concat - concatenates two strings into a new string
  *
  * @s1: first string passed
@@ -10,79 +71,24 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int k = 0;
-	int h = 0;
-	int j = 0;
-	int i;
-	int z;
-	static char *dup;
+	int i, v1, v2;
 
-	while (s1[j] != '\0')
-	{
-		k++;
-		j++;
-	}
+	v1 = _strlen(s1);
+	v2 = _strlen(s2);
+	static char *newStr;
 
-	j = 0;
-
-	while (s2[j] != '\0')
-	{
-		h++;
-		j++;
-	}
-
-	dup = (char *)malloc(sizeof(char) * (k + h + 1));
-
-	if (dup == NULL)
+	newStr = (char *)malloc(sizeof(char) * (v1 + v2 + 1));
+	if (newStr == NULL)
 	{
 		return ((char *)NULL);
 	}
-
-	if (k == 0 && h == 0)
+	if (s1)
 	{
-		dup[k] = '\0';
+		strcp(newStr, s1);
 	}
-	else
+	if (s2)
 	{
-		if (k == 0)
-		{
-			i = 0;
-
-			while (i <= h)
-			{
-				dup[i] = s2[i];
-				i++;
-			}
-		}
-		else if (h == 0)
-		{
-			i = 0;
-
-			while (i <= k)
-			{
-				dup[i] = s1[i];
-				i++;
-			}
-		}
-		else
-		{
-			i = 0;
-			z = 0;
-
-			while (i < k)
-			{
-				dup[i] = s1[i];
-				i++;
-			}
-			while (z < h)
-			{
-				dup[i] = s2[z];
-				z++;
-				i++;
-			}
-			dup[i] = '\0';
-		}
+		concat(newStr, s2);
 	}
-
-	return (dup);
+	return (newStr);
 }
