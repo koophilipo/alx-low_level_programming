6e1655fbd498eb3d9e89090a5484182e1bfd9511
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-13 20:09:03 +0000

    Commit init_dog program

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes elements of struct dog
+ *
+ * @d: struct pointer
+ * @name: string
+ * @age: float integer
+ * @owner: string
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
