84a1999984179292fbd29a584ecc487868891312
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-18 16:51:05 +0000

    Commit 2-main program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of file compiled from
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
