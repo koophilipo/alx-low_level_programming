4b9fb2f8fba3e2bd644ed55956f5cdb9f74a4676
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-19 18:39:29 +0000

    Commit 3-print_all function

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+#define END_ARRAY (format[i + 1])
+#define FLAGS (format[i] == 'c' || format[i] == 'i' \
+		|| format[i] == 'f' || format[i] == 's')
+
+/**
+ * print_s - print to std output
+ * @s: string
+ */
+int print_s(char *s)
+{
+	if (s != NULL)
+	{
+		printf("%s", s);
+		return (0);
+	}
+	printf("(nil)");
+	return (-1);
+}
+
+
+/**
+ * print_c - print char to std output
+ * @c: integer
+ */
+void print_c(int c)
+{
+	printf("%c", c);
+}
+
+/**
+ * print_i - print int to std output
+ * @i: integer
+ */
+void print_i(int i)
+{
+	printf("%d", i);
+}
+
+/**
+ * print_f - print string to std output
+ * @f: double
+ */
+void print_f(double f)
+{
+	printf("%f", f);
+}
+
+/**
+ * print_all - prints arguments to std output
+ * @format: constant string constant
+ * Return: Nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	va_list val;
+
+	va_start(val, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				print_c(va_arg(val, int));
+				break;
+			case 'i':
+				print_i(va_arg(val, int));
+				break;
+			case 'f':
+				print_f(va_arg(val, double));
+				break;
+			case 's':
+				print_s(va_arg(val, char*));
+				break;
+		}
+		if (END_ARRAY && FLAGS)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	va_end(val);
+	printf("\n");
+}
