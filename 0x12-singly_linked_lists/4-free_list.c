561a6a96c4ec36a7d9d0c88d82eedc16380acd52
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-26 23:46:57 +0000

    Correct free_list function and update list header

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,25 +7,15 @@
  */
 void free_list(list_t *head)
 {
-	list_t *nhead;
 	list_t *traverse;
 	list_t *temp;
 
-	nhead = head;
-
-	if (head != NULL)
+	traverse = head;
+	while (traverse != NULL)
 	{
-		while (head->next != NULL)
-		{
-			traverse = nhead;
-			while ((traverse->next)->next != NULL)
-			{
-				traverse = traverse->next;
-			}
-			temp = traverse->next;
-			traverse->next = NULL;
-			free(temp);
-		}
-		free(head);
+		temp = traverse;
+		traverse = traverse->next;
+		free(temp);
 	}
+
 }
