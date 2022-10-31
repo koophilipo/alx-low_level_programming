cbbb046bda2d4ed94d2aebe6882cf3505189d2e0
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-10-31 22:50:19 +0000

    Commmit set_bit function and update main.h header

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _pow1 - calculates power of a number
+ * @z: base number
+ * @p: power
+ * Return: integer
+ */
+int _pow1(unsigned int z, unsigned int p)
+{
+	int i = 0;
+	int b = z;
+	unsigned int comp = p;
+
+	if (p == 0)
+	{
+		return (1);
+	}
+	else if (p == 1)
+	{
+		return (b);
+	}
+	while (p > 1)
+	{
+		if (p == comp)
+		{
+			i = b * b;
+		}
+		else
+		{
+			i *= b;
+		}
+		--p;
+	}
+	return (i);
+}
+
+
+/**
+ * set_bit - sets the bit at index to 1
+ * @p: pointer to integer
+ * @index: index to set bit
+ * Return: 1 (success), -1 (fail)
+ */
+int set_bit(unsigned long int *p, unsigned int index)
+{
+	unsigned int j;
+
+	if (index > 39)
+	{
+		return (-1);
+	}
+	j = _pow1(2, index);
+	*p = *p | j;
+	return (j);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -15,5 +15,6 @@ int _putchar(int c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int index_range(unsigned long int n);
+int set_bit(unsigned long int *n, unsigned int index);
 
 #endif
