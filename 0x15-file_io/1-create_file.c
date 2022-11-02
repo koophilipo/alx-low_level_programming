3d9e30562dc0b6174cde0984a7c407b9c0047493
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-11-02 17:25:56 +0000

    Correct error in create_file function and update main.h header

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	j = open(filename, O_WRONLY | O_CREAT, mode);
+	j = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
 	if (j < 0)
 	{
 		return (-1);
