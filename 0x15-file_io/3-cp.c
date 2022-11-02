1510dc7176878c4031a6678f7118c701fcd1ef8a
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-11-02 23:30:51 +0000

    Correct error in cp program

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -75,11 +75,18 @@ void cp(const char *file1, const char *file2)
 	free(arr);
 }
 
+/**
+ * main - duplicates a file
+ * @ac: number of arguments
+ * @av: array of arguments
+ * Return: Always 0.
+ */
+
 int main(int ac, char **av)
 {
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", av[1], av[2]);
+		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", "file_from", "file_to");
 		exit(97);
 	}
 	cp(av[1], av[2]);
