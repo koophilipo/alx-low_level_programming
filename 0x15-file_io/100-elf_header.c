d31603142bdf691bf29547ad0a8faf9d1700743e
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-11-04 14:56:16 +0000

    Commit elf_header program and remove readelf

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,105 +1,43 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <string.h>
 
-#define TEMP "._temp_.tmp"
 
-/**
- * main - read elf header
- * @ac: number of arguments
- * @av: pointer to arguments
- * Return: Always 0.
- */
-int main(int ac, char **av)
+int _strlen(const char *str)
 {
-	int i, j, buff_len = 30;
+	int len = 0, i = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
+
+
+int main(int ac __attribute__((unused)), char **av)
+{
+	int i, c = 0;
+
+	char *comm = "readelf -h ";
+	char *pipe = " | head -11";
 	char *str;
-	char *cmdread = "readelf -h ";
-	char *pipe = " > ._temp_.tmp";
-	char *file = av[1];
-	FILE *fd = NULL;
 
-	if (ac != 2)
-	{
-		dprintf(STDERR_FILENO, "Usage: readelf --help\n");
-		exit(97);
-	}
-	if (av[1][0] == '\0')
-	{
-		dprintf(STDERR_FILENO, "No valid file specified\n");
-		exit(98);
-	}
-	str = (char *)malloc(sizeof(char) * 30);
-	if (str == NULL)
-	{
-		printf("malloc failed");
-		exit(104);
-	}
-	printf("we are at malloc\n");
-	for (i = 0; i < 10; i++)
-	{
-		printf("assign1\n");
-		str[i] = cmdread[i];
-		printf("assign2");
-	}
-	printf("we are after cmd read");
-	for (i = i; file[j] != '\0'; j++, i++)
-	{
-		if (i >= buff_len)
-		{
-			buff_len += 30;
-			str = realloc(str, buff_len);
-			if (str == NULL)
-			{
-				dprintf(STDERR_FILENO, "Realloc Failed\n");
-				exit(99);
-			}
-		}
-		str[i] = file[j];
-	}
-	printf("We are at read\n");
-	for (j = 0; pipe[j] != '\0'; j++)
-	{
-		if (i >= buff_len)
-		{
-			buff_len += 20;
-			str = realloc(str, buff_len);
-			if (str == NULL)
-			{
-				dprintf(STDERR_FILENO, "Realloc failed\n");
-				exit(100);
-			}
-		}
-		str[i] = pipe[j];
-	}
-	str[i] = '\0';
-	j = system(str);
-	if (j < 0)
-	{
-		dprintf(STDERR_FILENO, "Command Failed\n");
-		exit(101);
-	}
-	fd = fopen(TEMP, "r");
-	if (fd == NULL)
-	{
-		dprintf(STDERR_FILENO, "File open failed\n");
-		exit (102);
-	}
-	j = 0;
-	while (j < 11)
-	{
-		char ch = fgetc(fd);
-		printf("%c", ch);
-		if (ch == '\n')
-		{
-			j++;
-		}
-	}
-	printf("we are at command\n");
-	system("rm ._temp_.tmp");
-	free(str);
+	c += _strlen(comm);
+	c += _strlen(av[1]);
+	c += _strlen(pipe);
+	str = (char *)malloc(sizeof(char) * (c + 1));
+	strcat(str, comm);
+	strcat(str, av[1]);
+	strcat(str, pipe);
 
+	i = system(str);
+	if (i < 0)
+	{
+		printf("command failed\n");
+	}
 	return (0);
 }
