9133ad8d7db3db2fb7d3f75f09db72205df17c7c
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-11 16:17:43 +0000

    Commit add_dnodeint function and corresponding test program

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to first node
+ * @idx: position to insert node
+ * @n: value to insert new node
+ *
+ * Return: new node pointer (success), NULL (fail)
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *traverse, *new_node;
+
+	new_node = (dlistint_t *)calloc(1, sizeof(dlistint_t));
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	traverse = *h;
+	new_node->n = n;
+	for (i = 0; i <= idx && traverse != NULL; i++)
+	{
+		if (i == idx)
+		{
+			if (traverse->prev == NULL)
+			{
+				add_dnodeint(h, n);
+			}
+			else
+			{
+				new_node->prev = traverse->prev;
+				new_node->next = traverse;
+				traverse->prev = new_node;
+				new_node->prev->next = new_node;
+			}
+			return (new_node);
+		}
+		traverse = traverse->next;
+	}
+	return (NULL);
+}
