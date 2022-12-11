82a4b32b0cf697bd85f6b5662df7cb0c8b5ee9ce
koophilipo <111448452+koophilipo@users.noreply.github.com>
2022-12-11 15:22:55 +0000

    Commit get_dnodeint function and corresponding test program

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a linked list
+ * @head: pointer to the first node
+ * @index: the position of the node to remove
+ *
+ * Return: node removed (success), NULL (fail)
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *traverse;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	traverse = head;
+	for (i = 0; i <= index; i++)
+	{
+		if (i == index)
+		{
+			return (traverse);
+		}
+		traverse = traverse->next;
+	}
+	return (NULL);
+}
